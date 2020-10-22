e0f5e47f2cdce7fd064a3c878353eb49bf46f956
maxhudson <max@henrybuilt.com>
2020-10-22 14:08:04 -0700

    error message

--- src/lib/post/post.js
+++ src/lib/post/post.js
@@ -30,7 +30,7 @@ module.exports = ({app, db, auth}) => function(path, callback, {requireUser=true
     }
 
     try {
-      if (requireUser && !(user && user.id)) throwError({message: 'Something went wrong #100', key: 'userRequired'});
+      if (requireUser && !(user && user.id)) throwError({message: 'Your session is invalid or has expired. Please try logging in again.', key: 'userRequired'});
 
       var data = await callback({...request.body, user, body: request.body, throwError});
 
