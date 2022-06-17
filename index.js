c499bee52fe6b7132d51acb8b5ff667ce44055a0
maxhudson <max@henrybuilt.com>
2022-06-17 16:15:20 -0700

    extended option

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ var api = {
     const app = express();
 
     app.use(express.json({limit: '50mb'}));
-    app.use(express.urlencoded({limit: '50mb'}));
+    app.use(express.urlencoded({limit: '50mb', extended: false}));
     app.use(cors());
     app.use(passport.initialize());
     app.use((request, response, next) =>  {
