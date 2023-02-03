edf5b4a343b2c57c1bec2eca96f9248ab7c020f0
maxhudson <max@henrybuilt.com>
2023-02-03 15:46:43 -0800

    max file size

--- index.js
+++ index.js
@@ -24,8 +24,8 @@ var api = {
     //< server init
     const app = express();
 
-    app.use(express.json({limit: '50mb'}));
-    app.use(express.urlencoded({limit: '50mb', extended: false}));
+    app.use(express.json({limit: '500mb'}));
+    app.use(express.urlencoded({limit: '500mb', extended: false}));
     app.use(cors());
     app.use(passport.initialize());
     app.use((request, response, next) =>  {
